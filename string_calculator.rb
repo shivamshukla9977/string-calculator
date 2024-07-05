05adca15e138adfff2920cf8828cd0c25becb83c
shivam <skshukla9977@gmail.com>
2024-07-05 13:25:47 +0530

    Fixed failing test for case 2

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,5 +2,8 @@ class StringCalculator
   def self.add(string)
     # Return 0 if the input string is empty
     return 0 if string.empty?
+
+    # Return number itself if the input string is single value
+    return string.to_i if string.length.eql?(1)
   end
 end
