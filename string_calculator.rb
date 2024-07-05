f8c7f9c0ccb06358441a8516b4d441962b6cd27b
shivam <skshukla9977@gmail.com>
2024-07-05 13:28:17 +0530

    Fixed failing test for case 3

--- string_calculator.rb
+++ string_calculator.rb
@@ -5,5 +5,8 @@ class StringCalculator
 
     # Return number itself if the input string is single value
     return string.to_i if string.length.eql?(1)
+
+    # Return sum of numbers seperated with commas
+    string.split('').map(&:to_i).sum
   end
 end
