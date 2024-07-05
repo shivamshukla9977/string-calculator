7b1ee7e757a6b8e6601b13d0a366be86b422ad50
shivam <skshukla9977@gmail.com>
2024-07-05 13:20:14 +0530

    Added test case for when input string is empty

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -3,4 +3,13 @@
 require_relative '../string_calculator'
 
 RSpec.describe StringCalculator do
+  describe '.add' do
+    context 'when given an empty string' do
+      it 'returns 0' do
+        expect(StringCalculator.add('')).to eq(0)
+      end
+    end
+  end
+
+  # More test cases will be added here as we progress with the development.
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -1,2 +1,5 @@
 class StringCalculator
+  def self.add(string)
+    # implementation will go here
+  end
 end
