9d7faf5c721802ff3800606fb710955c5887a4c0
shivam <skshukla9977@gmail.com>
2024-07-05 13:30:27 +0530

    Added test for when string has different delimeters as  input

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -27,5 +27,11 @@ RSpec.describe StringCalculator do
         expect(StringCalculator.add("1\n2,3")).to eq(6)
       end
     end
+
+    context 'when given a different delimiter' do
+      it 'returns the sum of the numbers' do
+        expect(StringCalculator.add("//;\n1;2")).to eq(3)
+      end
+    end
   end
 end
